f5852040850fdc7ae10564b8a3f7f8f17dfeb3a0
deesmithy <89953680+deesmithy@users.noreply.github.com>
2024-07-01 09:53:27 -0600

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Donovan Smith on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
